8f55cdc9b66f22d6aa040414b810d5bc3c6c4b35
Hector Ordonez <hjavierog@gmail.com>
2023-02-28 16:18:00 +0200

    Finished valid brackets kata

--- src/ValidBraces/Main.php
+++ src/ValidBraces/Main.php
@@ -10,11 +10,35 @@ class Main
      * String might contain {, [ and ( and the function should return true if these are closed in the correct order.
      * For instance {}, [] and [{}] are correct, but {[}] is not.
      *
-     * @param string $str
+     * @param string $braces
      * @return bool
      */
-    public function isValid(string $str): bool
+    public function isValid(string $braces): bool
     {
+        $validBraces = [
+            ')' => '(',
+            ']' => '[',
+            '}' => '{',
+        ];
+
+        $last = [];
+
+        foreach (str_split($braces) as $char) {
+            if (array_key_exists($char, $validBraces)) {
+                if (count($last) > 0 && $last[count($last) - 1] === $validBraces[$char]) {
+                    array_pop($last);
+                } else {
+                    return false;
+                }
+            } else {
+                $last[] = $char;
+            }
+        }
+
+        if (count($last) > 0) {
+            return false;
+        }
         return true;
     }
+
 }
--- src/ValidBraces/tests/MainTest.php
+++ src/ValidBraces/tests/MainTest.php
@@ -51,4 +51,90 @@ class MainTest extends TestCase
         // Assert
         $this->assertSame(true, $result);
     }
+
+    /**
+     * @test
+     */
+    public function it_returns_false_when_it_starts_with_closing_char()
+    {
+        // Arrange
+        $str = '}';
+
+        // Act
+        $result = (new Main())->isValid($str);
+
+        // Assert
+        $this->assertFalse($result);
+    }
+
+    /**
+     * @test
+     */
+    public function it_returns_true_when_opening_and_closing_twice_in_correct_order()
+    {
+        // Arrange
+        $str = '(){}';
+
+        // Act
+        $result = (new Main())->isValid($str);
+
+        // Assert
+        $this->assertTrue($result);
+    }
+
+    /**
+     * @test
+     */
+    public function it_returns_true_when_opening_and_closing_one_inside_the_other()
+    {
+        // Arrange
+        $str = '({})';
+
+        // Act
+        $result = (new Main())->isValid($str);
+
+        // Assert
+        $this->assertTrue($result);
+    }
+
+    /**
+     * @test
+     */
+    public function it_returns_false_when_multiple_incorrect_ones()
+    {
+        // Arrange
+        $str = '[({})](]';
+
+        // Act
+        $result = (new Main())->isValid($str);
+
+        // Assert
+        $this->assertFalse($result);
+    }
+
+    /**
+     * @dataProvider examples
+     * @test
+     */
+    public function it_returns_when_incorrect_ones($str, $expected)
+    {
+        // Act
+        $result = (new Main())->isValid($str);
+
+        // Assert
+        $this->assertSame($expected, $result);
+    }
+
+    public function examples()
+    {
+        return [
+            ["(){}[]", true],
+            ["([{}])", true],
+            ["(}", false],
+            ["[(])", false],
+            ["[({})](]", false],
+            ["((((()))))", true],
+            ["(((((())))", false],
+        ];
+    }
 }
